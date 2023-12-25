322e409fd4859a79413f9a7cb12ee7f9149241ea
Subroto Das <rupashi.bangla.club@gmail.com>
2023-12-25 16:33:03 +0600

    Functionility adding

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { PortNumber, MONGODB_URL, ACCESS_TOKEN_KEY_VALUE } = require("./secret");
 //middleWare
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -36,7 +36,7 @@ const mongodbConnection = async () => {
 };
 
 //Mongodb Document And Collection Setup
-const taskslist = client.db("task-management").collection("taskslist");
+const tasklists = client.db("task-management").collection("taskliss");
 
 //MiddleWare
 const VerifyToken = (req, res, next) => {
@@ -52,10 +52,66 @@ const VerifyToken = (req, res, next) => {
     next();
   });
 };
+//UsersPost Controller
+app.post("/api/users", (req, res, next) => {
+  try {
+    const users = req.body;
+    const token = jwt.sign(users, ACCESS_TOKEN_KEY_VALUE, { expiresIn: "24h" });
+    if (token) {
+      res.status(200).send({ token });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-//
-app.get("/", (req, res) => {
-  res.send("Welcome to the Task Management Server");
+//Task Get Controller
+app.get("/api/tasks", VerifyToken, async (req, res, next) => {
+  const email = req?.query?.email;
+  if (email !== req?.decoded?.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  const resData = await tasklists.find({ email }).toArray();
+  return res.status(200).send(resData.reverse());
+});
+//Task GetBy ID Controller
+app.get("/api/tasks/:id", async (req, res, next) => {
+  const id = req?.params?.id;
+  const resData = await tasklists.findOne({ _id: new ObjectId(id) });
+  return res.status(200).send(resData);
+});
+
+//Task Delete Controller
+app.delete("/api/tasks/delete/:id", VerifyToken, async (req, res, next) => {
+  const email = req?.query?.email;
+  if (email !== req?.decoded?.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  const id = req?.params?.id;
+  const resDeleteData = await tasklists.deleteOne({ _id: new ObjectId(id) });
+  return res.status(200).send(resDeleteData);
+});
+
+//Creating  Task Update Request
+app.patch("/api/task/update/:id", VerifyToken, async (req, res, next) => {
+  const email = req?.query?.email;
+  if (email !== req?.decoded?.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  const id = req?.params?.id;
+  const filter = { _id: new ObjectId(id) };
+  const query = { $set: req?.body };
+  const createInfo = await tasklists.updateOne(filter, query);
+  return res.status(200).send(createInfo);
+});
+//Creating  Task Post Request
+app.post("/api/task/create", VerifyToken, async (req, res, next) => {
+  const email = req?.query?.email;
+  if (email !== req?.decoded?.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  const createInfo = await tasklists.insertOne(req?.body);
+  return res.status(200).send(createInfo);
 });
 
 //
